35c7ef105bf1030f9403948e1f76730a3c21eca3
Kuldeep-Kumar-Sharma <sharman94kuldeep@gmail.com>
2021-06-30 23:59:31 +0530

    added

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
-import { Container, Card, Nav, Col, Row, Button } from "react-bootstrap";
+import { Container, Card, Nav, Image, Col, Row, Button } from "react-bootstrap";
 import { Wrapper } from "./styles.js";
 
 const Layout = ({ pageTitle, children }) => {
@@ -17,22 +17,12 @@ const Layout = ({ pageTitle, children }) => {
   return (
     <Wrapper>
       {/* Stack the columns on mobile by making one full-width and the other half-width */}
-      <Row>
-        <Col xs={6} md={3} className="justify-content-md-center">
-          <Card style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://avatars.githubusercontent.com/u/39633593?s=400&u=700c7cf01a90c7047c80d7a9640573ca8b05c320&v=4"
-            />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
+      <Row className="justify-content-md-center">
+        <Col xs={6} md={3}>
+          <Image
+            src="https://avatars.githubusercontent.com/u/39633593?s=400&u=700c7cf01a90c7047c80d7a9640573ca8b05c320&v=4"
+            rounded
+          />
         </Col>
         <Col className="background1" xs={12} md={9}>
           xs=12 md=8
